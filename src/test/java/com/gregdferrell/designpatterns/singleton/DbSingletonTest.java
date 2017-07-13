e506c82e4f848d9de793e5ca5a34e4a2d1d5c9d6
Greg Ferrell <gferrell20@gmail.com>
2017-07-12 22:50:11 -0300

    finish the singleton impl and test

--- src/test/java/com/gregdferrell/designpatterns/singleton/DbSingletonTest.java
+++ src/test/java/com/gregdferrell/designpatterns/singleton/DbSingletonTest.java
@@ -7,7 +7,10 @@ import org.junit.rules.TestName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class DbSingletonTest {
 
@@ -27,10 +30,29 @@ public class DbSingletonTest {
     }
 
     @Test()
-    public void testSingletonSameMemAddress() {
+    public void testGetInstanceReturnsSameInstance() {
         DbSingleton instanceOne = DbSingleton.getInstance();
         DbSingleton instanceTwo = DbSingleton.getInstance();
 
         assertEquals(instanceOne.toString(), instanceTwo.toString());
+    }
+
+    @Test()
+    public void testGetDbConnectionReturnsSameInstanceAndPerformsOnSubsequentCalls() {
+        DbSingleton instanceOne = DbSingleton.getInstance();
+        DbSingleton instanceTwo = DbSingleton.getInstance();
+
+        long before = System.currentTimeMillis();
+        Connection connectionOne = instanceOne.getConnection();
+        long after = System.currentTimeMillis();
+        long diff1 = after - before;
+
+        before = System.currentTimeMillis();
+        Connection connectionTwo = instanceTwo.getConnection();
+        after  = System.currentTimeMillis();
+        long diff2 = after - before;
+
+        assertEquals(connectionOne.toString(), connectionTwo.toString());
+        assertTrue(diff1 > diff2);
     }
 }
